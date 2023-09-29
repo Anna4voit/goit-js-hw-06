7052330c8fd7d8f46a6b07b2c8e460b63405a369
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2023-09-29 22:31:07 +0300

    update_task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,31 +1,41 @@
 //отримую кнопку за дата атрибутом
 const buttonDecrement = document.querySelector(
-  '#counter button[data-action="decrement"]'
+  'button[data-action="decrement"]'
 );
 const buttonIncrement = document.querySelector(
-  '#counter button[data-action="increment"]'
+  'button[data-action="increment"]'
 );
+//початкове значення лічильника
+let counterValue = 0;
 //отримую значення лічильника
 const counter = document.querySelector('#value');
+//функція, що по кліку зменшує на одиницю лічильник
+// function clickDecrement(event) {
+//   counterValue -= 1;
+//   counter.textContent = counterValue;
+//   //console.log(counter.textContent);
+//   //   console.log(counterValue);
+// }
+//або через стрілку
+const clickDecrement = event => {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+};
+//функція, що по кліку збільшує на одиницю лічильник
+// function clickIncrement(event) {
+//   counterValue += 1;
+//   counter.textContent = counterValue;
+//   //console.log(counter.textContent);
+//   //   console.log(counterValue);
+// }
+//або через стрілку
+const clickIncrement = event => {
+  counterValue += 1;
+  counter.textContent = counterValue;
+};
 //на кнопку навіщую слухача події по кліку з відповідними callback функціями
 buttonDecrement.addEventListener('click', clickDecrement);
 buttonIncrement.addEventListener('click', clickIncrement);
-//початкове значення лічильника
-let counterValue = 0;
-//функція, що по кліку зменшує на одиницю лічильник
-function clickDecrement(event) {
-  counterValue -= 1;
-  counter.textContent = `${counterValue}`;
-  //console.log(counter.textContent);
-  //   console.log(counterValue);
-}
-//функція, що по кліку збільшує на одиницю лічильник
-function clickIncrement(event) {
-  counterValue += 1;
-  counter.textContent = `${counterValue}`;
-  //console.log(counter.textContent);
-  //   console.log(counterValue);
-}
 
 //а можна зробити взагалі цікаво: звернутися до усіх кнопок і в циклі навішати на кожну подію
 //const buttons = document.querrySelectorAll('button');
